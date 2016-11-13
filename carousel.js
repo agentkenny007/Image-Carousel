c27d2d61dd349dd7d969297378a2e5de3a736db6
Ikenna <ikennaugwuh@gmail.com>
2016-11-13 18:22:34 -0500

    Fixed broken links.

--- carousel.js
+++ carousel.js
@@ -1,37 +1,37 @@
 var carouselImages = [
   {
-    image_url: 'http://desktopography.net/media/exhibition/2006/hushed/large.jpg',
-    title: 'Hushed',
-    photographer_id: 'Michael Withers'
+    image_url: 'https://drscdn.500px.org/photo/133812917/m%3D900/42b4012ff384fe169c95b6290b1a0a40',
+    title: 'Lake O\'Hara, BC, Canada',
+    photographer_id: 'Christian Thamm'
   },
   {
-    image_url: 'http://desktopography.net/media/exhibition/2006/dndln/large.jpg',
-    title: 'Dndln',
-    photographer_id: 'Carlos'
+    image_url: 'https://drscdn.500px.org/photo/153575153/m%3D900/310df587a7aca0c9b4010b47f61c83bb',
+    title: 'Lake Braies, Italy',
+    photographer_id: 'Eva Lechner'
   },
   {
-    image_url: 'http://desktopography.net/media/exhibition/2007/open_source/large.jpg',
-    title: 'Open Source',
-    photographer_id: 'Jerico Santander'
+    image_url: 'https://drscdn.500px.org/photo/71485645/m%3D900/248ad2c43e75b40a20ffa3a4ce76dd08',
+    title: 'Geiranger, Norway',
+    photographer_id: 'Max Rive'
   },
   {
-    image_url: 'http://desktopography.net/media/exhibition/2007/treehugger/large.jpg',
-    title: 'Treehugger',
-    photographer_id: 'Pete Harrison'
+    image_url: 'https://drscdn.500px.org/photo/74647277/m%3D900/f5808ebfec45272e05718c86988c2a0a',
+    title: 'Coron Island, Philippines',
+    photographer_id: 'KEvin Boutwell'
   },
   {
-    image_url: 'http://desktopography.net/media/exhibition/2007/dsn/large.jpg',
-    title: 'DSN',
-    photographer_id: 'Marius Bauer'
+    image_url: 'https://drscdn.500px.org/photo/99427685/m%3D900/cfbe15a487e3d02e757009ac555e1b48',
+    title: 'Mojave Dessert, USA',
+    photographer_id: 'Marc Adamus'
   },
   {
-    image_url: 'http://desktopography.net/media/exhibition/2008/blue_moon/large.jpg',
-    title: 'Blue Moon',
-    photographer_id: 'Microbot'
+    image_url: 'https://drscdn.500px.org/photo/125850765/m%3D900/036abde9782c2587a5bb7fd23a29e0bc',
+    title: 'Geiranger, Norway',
+    photographer_id: 'Igor Sukmansky'
   },
   {
-    image_url: 'http://desktopography.net/media/exhibition/2009/fight_for_alpha/large.jpg',
-    title: 'Fight for Alpha',
-    photographer_id: 'Space Jump'
+    image_url: 'https://drscdn.500px.org/photo/75148323/m%3D900/73542915cd892d12bb8cc9a506dbfb99',
+    title: 'Sunrise in Middle Earth',
+    photographer_id: 'Thomas Mueller'
   }
 ];
